bc59067ec34ed97920a7e017e9c593e078223c7a
robinsonuq <86636627+robinsonuq@users.noreply.github.com>
2024-02-29 12:57:32 -0500

    coomit test estudiante

--- universidad/src/main/java/co/edu/uniquindio/poo/App.java
+++ universidad/src/main/java/co/edu/uniquindio/poo/App.java
@@ -7,11 +7,6 @@ package co.edu.uniquindio.poo;
 public class App {
     public static void main(String[] args) {
         
-       Perro miPerrito = new Perro("Anyelo", "pitbull");
-
-       for (int i = 0; i <=3; i++) {
-        miPerrito.caminar(30);
-       }
 
     }
 }
